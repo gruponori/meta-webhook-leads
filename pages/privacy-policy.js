3c08fe26c1a40bc15451c57aa7b01a01d598b27e
gruponori <gnorimkt@gmail.com>
2025-09-27 14:40:23 -0300

    Update privacy-policy.js

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -2,8 +2,18 @@ export default function PrivacyPolicy() {
   return (
     <main style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
       <h1>Política de Privacidade</h1>
-      <p>Esta é a política de privacidade do nosso aplicativo que utiliza o webhook de leads do Facebook...</p>
-      <p>Inclua aqui informações claras sobre o uso de dados dos usuários conforme necessário.</p>
+      <p>
+        Esta aplicação coleta dados enviados pelo formulário de leads do Facebook para fins de contato comercial.
+        Nenhum dado será compartilhado com terceiros fora dos responsáveis pela empresa ou produto.
+      </p>
+      <p>
+        Os dados coletados são usados somente conforme especificado no propósito do formulário.
+        Para mais informações, entre em contato com os responsáveis através dos canais oficiais.
+      </p>
+      <p>
+        Última atualização: 27/09/2025
+      </p>
     </main>
-  )
+  );
 }
+
