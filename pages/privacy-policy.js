e005765793c1a6ef92314e52bd8489dab54dc8ac
gruponori <gnorimkt@gmail.com>
2025-09-27 14:47:40 -0300

    Update privacy-policy.js

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -3,16 +3,11 @@ export default function PrivacyPolicy() {
     <main style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
       <h1>Política de Privacidade</h1>
       <p>
-        Esta aplicação coleta dados enviados pelo formulário de leads do Facebook para fins de contato comercial.
-        Nenhum dado será compartilhado com terceiros fora dos responsáveis pela empresa ou produto.
+        Esta aplicação coleta dados enviados por usuários através dos formulários de lead do Facebook,
+        utilizados exclusivamente para contato e informações solicitadas. Não compartilhamos dados com terceiros.
+        Para mais informações, entre em contato com nossa equipe.
       </p>
-      <p>
-        Os dados coletados são usados somente conforme especificado no propósito do formulário.
-        Para mais informações, entre em contato com os responsáveis através dos canais oficiais.
-      </p>
-      <p>
-        Última atualização: 27/09/2025
-      </p>
+      <p>Última atualização: 27/09/2025</p>
     </main>
   );
 }
